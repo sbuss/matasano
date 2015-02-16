3344938f883298bcccd8d8222b5a3827fef90075
Steven Buss <steven.buss@gmail.com>
2015-02-15 21:13:48 -0800

    Helper method to get bytes from hex string

--- hex_utils.py
+++ hex_utils.py
@@ -2,5 +2,9 @@ import base64
 import binascii
 
 
+def hex_to_bytes(hex_str):
+    return binascii.a2b_hex(hex_str)
+
+
 def hex_to_b64(hex_str):
-    return base64.b64encode(binascii.a2b_hex(hex_str))
+    return base64.b64encode(hex_to_bytes(hex_str))
