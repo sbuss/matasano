9fb22dce3a3411e5743df530f7279422f2574359
Steven Buss <steven.buss@gmail.com>
2015-02-15 21:16:31 -0800

    docs

--- hex_utils.py
+++ hex_utils.py
@@ -3,8 +3,15 @@ import binascii
 
 
 def hex_to_bytes(hex_str):
+    """Convert a hex string to bytes."""
     return binascii.a2b_hex(hex_str)
 
 
 def hex_to_b64(hex_str):
+    """Convert a hex string to a base64 string.
+    Args:
+        hex_str: The hex string you want to convert. If this is already a hex
+            bytes array it'll fail, since python can't really tell the
+            difference.
+    """
     return base64.b64encode(hex_to_bytes(hex_str))
