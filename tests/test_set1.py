9043f39c2f2ef96a39e06dd3d96961fc36ecfd6e
Steven Buss <steven.buss@gmail.com>
2015-02-16 10:21:34 -0800

    Use a single testcase class

--- tests/test_set1.py
+++ tests/test_set1.py
@@ -3,9 +3,8 @@ from unittest import TestCase
 import hex_utils
 
 
-class TestHexToBase64(TestCase):
-    # Challenge 1
-    def test_h2b64(self):
+class TestSet1(TestCase):
+    def test_challenge1_h2b64(self):
         inp = ("49276d206b696c6c696e6720796f75722"
                "0627261696e206c696b65206120706f69"
                "736f6e6f7573206d757368726f6f6d")
@@ -13,10 +12,7 @@ class TestHexToBase64(TestCase):
                "WtlIGEgcG9pc29ub3VzIG11c2hyb29t")
         self.assertEqual(hex_utils.hex_to_b64(inp), out)
 
-
-class TestHexxor(TestCase):
-    # Challenge 2
-    def test_hexxor(self):
+    def test_challenge2_hexxor(self):
         hex1 = "1c0111001f010100061a024b53535009181c"
         hex2 = "686974207468652062756c6c277320657965"
         out = "746865206b696420646f6e277420706c6179"
