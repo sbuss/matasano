93043a0aeabbc8fa9c99e0a0d28e7fcd614e8db1
Steven Buss <steven.buss@gmail.com>
2015-02-15 21:33:24 -0800

    1.2 - hexxor

--- hex_utils.py
+++ hex_utils.py
@@ -15,3 +15,20 @@ def hex_to_b64(hex_str):
             difference.
     """
     return base64.b64encode(hex_to_bytes(hex_str))
+
+
+def hex_to_int(hex_str):
+    """Convert a hex string to an int"""
+    return int(hex_str, 16)
+
+
+def int_to_hex(hex_int):
+    """Convert an integer into a hex string"""
+    return format(hex_int, 'x')
+
+
+def hexxor(hex_str1, hex_str2):
+    hex1 = hex_to_int(hex_str1)
+    hex2 = hex_to_int(hex_str2)
+    xor = hex1 ^ hex2
+    return int_to_hex(xor)
--- tests/test_set1.py
+++ tests/test_set1.py
@@ -11,3 +11,12 @@ class TestHexToBase64(TestCase):
         out = ("SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsa"
                "WtlIGEgcG9pc29ub3VzIG11c2hyb29t")
         self.assertEqual(hex_utils.hex_to_b64(inp), out)
+
+
+class TestHexxor(TestCase):
+    # Challenge 2
+    def test_hexxor(self):
+        hex1 = "1c0111001f010100061a024b53535009181c"
+        hex2 = "686974207468652062756c6c277320657965"
+        out = "746865206b696420646f6e277420706c6179"
+        self.assertEqual(hex_utils.hexxor(hex1, hex2), out)
