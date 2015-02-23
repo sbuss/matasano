f242a1fb248fa51abc8b11c8207c49114f159d27
Steven Buss <steven.buss@gmail.com>
2015-02-22 16:40:43 -0800

    Stripping input might be cheating, but I can't see a different solution.

--- matasano/profile_utils.py
+++ matasano/profile_utils.py
@@ -29,9 +29,9 @@ def decode_profile(kv_str):
     if any(len(item) != 1 for item in parsed_qs.values()):
         raise ValueError("Repeated keys are not allowed, cheater!")
     return OrderedDict([
-        ('email', parsed_qs['email'][0]),
-        ('uid', int(parsed_qs['uid'][0])),
-        ('role', parsed_qs['role'][0]),
+        ('email', parsed_qs['email'][0].strip()),
+        ('uid', int(parsed_qs['uid'][0].strip())),
+        ('role', parsed_qs['role'][0].strip()),
     ])
 
 
